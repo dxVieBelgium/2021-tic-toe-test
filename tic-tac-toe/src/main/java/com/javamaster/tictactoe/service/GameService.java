304882a1f843af741b6cc6f001e21cf640080aac
dxVieBelgium <dexter.benoni@akka.eu>
2021-11-18 08:38:20 +0100

    feat: implement gameplay and verifyWinner process

--- tic-tac-toe/src/main/java/com/javamaster/tictactoe/service/GameService.java
+++ tic-tac-toe/src/main/java/com/javamaster/tictactoe/service/GameService.java
@@ -6,8 +6,10 @@ import com.javamaster.tictactoe.exception.InvalidGameException;
 import com.javamaster.tictactoe.exception.InvalidParamException;
 import com.javamaster.tictactoe.exception.NotFoundException;
 import com.javamaster.tictactoe.model.Game;
+import com.javamaster.tictactoe.model.GamePlay;
 import com.javamaster.tictactoe.model.GameStatus;
 import com.javamaster.tictactoe.model.Player;
+import com.javamaster.tictactoe.model.TicToe;
 import com.javamaster.tictactoe.storage.GameStorage;
 
 import org.springframework.stereotype.Service;
@@ -63,4 +65,63 @@ public class GameService {
 
         return game;
     }
+
+    public Game gamePlay(GamePlay gamePlay) throws NotFoundException, InvalidGameException {
+
+        if(!GameStorage.getInstance().getGames().containsKey(gamePlay.getGameId())) {
+            throw new NotFoundException("Game not found");
+        }
+
+        Game game = GameStorage.getInstance().getGames().get(gamePlay.getGameId());
+
+        if(game.getStatus().equals(GameStatus.FINISHED)){
+            throw new InvalidGameException("Game is already finished");
+        }
+
+        int [][] board = game.getBoard();
+        board[gamePlay.getCoordinateX()][gamePlay.getCoordinateY()] = gamePlay.getType().getValue();
+
+        checkWinner(game.getBoard(), TicToe.X);
+        checkWinner(game.getBoard(), TicToe.O);
+
+        GameStorage.getInstance().setGame(game);
+        return game;
+    }
+
+    private Boolean checkWinner(int [][] board, TicToe ticToe) {
+
+        int [] boardArray = new int[9];
+        int counterIndex = 0;
+
+        for (int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[i].length; j++) {
+                boardArray[counterIndex] = board[i][j];
+                counterIndex++;
+            }
+        }
+
+        int [][] winCombinations = {
+            {0, 1, 2}, {3,4,5}, 
+            {6,7,8},{0,3,6}, 
+            {1,4,7}, {2,5,8},
+            {0,4,8}, {2,4,6}
+        };
+
+        for(int i=0; i < winCombinations.length; i++) {
+            int counter = 0;
+
+            for(int j = 0; j < winCombinations[i].length; j++) {
+
+                if(boardArray[winCombinations[i][j]] == ticToe.getValue()){
+                    counter++;
+
+                    if(counter == 3) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
 }
