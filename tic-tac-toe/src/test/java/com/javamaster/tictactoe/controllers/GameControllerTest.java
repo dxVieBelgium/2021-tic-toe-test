9582c58ce8193a800c95f73ef6fee3f9172c2e3e
dxVieBelgium <dexter.benoni@akka.eu>
2021-11-20 22:29:32 +0100

    test: fix issue on game creation test

--- tic-tac-toe/src/test/java/com/javamaster/tictactoe/controllers/GameControllerTest.java
+++ tic-tac-toe/src/test/java/com/javamaster/tictactoe/controllers/GameControllerTest.java
@@ -4,40 +4,51 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.javamaster.tictactoe.model.Game;
 import com.javamaster.tictactoe.model.Player;
 import com.javamaster.tictactoe.service.GameService;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 
 @ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
+@SpringBootTest
 public class GameControllerTest {
 
     @Autowired
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
 
     @MockBean
     private GameService gameService;
     
     @Test
     void start() throws Exception {
+
         Player player = new Player();
 
         player.setLogin("eth0");
 
+        when(gameService.createGame(player)).thenCallRealMethod();
+
         mockMvc.perform(
                 MockMvcRequestBuilders
-                        .post("/start")
+                        .post("/game/start")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(asJsonString(player))
-        ).andExpectAll(status().isOk());
+        ).andDo(print()).andExpect(status().isOk()).andExpect(MockMvcResultMatchers.jsonPath("$.gameId").exists()).andReturn();
     }
 
     public static String asJsonString(Object obj) {
