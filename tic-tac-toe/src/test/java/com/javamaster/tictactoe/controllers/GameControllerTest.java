ac1f7feab42044d04d10ed0ef8e56e7b363bbe2c
dxVieBelgium <dexter.benoni@akka.eu>
2021-11-22 21:32:36 +0100

    wip: test controller

--- tic-tac-toe/src/test/java/com/javamaster/tictactoe/controllers/GameControllerTest.java
+++ tic-tac-toe/src/test/java/com/javamaster/tictactoe/controllers/GameControllerTest.java
@@ -1,15 +1,18 @@
 package com.javamaster.tictactoe.controllers;
 
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.javamaster.tictactoe.controller.dto.ConnectRequest;
 import com.javamaster.tictactoe.model.Game;
 import com.javamaster.tictactoe.model.Player;
 import com.javamaster.tictactoe.service.GameService;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -51,6 +54,22 @@ public class GameControllerTest {
         ).andDo(print()).andExpect(status().isOk()).andExpect(MockMvcResultMatchers.jsonPath("$.gameId").exists()).andReturn();
     }
 
+    @Test()
+    void should_not_connect_when_id_not_exist() throws Exception {
+
+        ConnectRequest connectRequest = new ConnectRequest();
+        connectRequest.setPlayer(new Player("onimaru"));
+        connectRequest.setGameId("5e16f5d4-1a38-4322-9744-d5bf9ff9a5c1");
+
+        when(gameService.connectToGame(connectRequest.getPlayer(), connectRequest.getGameId())).thenCallRealMethod().thenThrow();
+
+        mockMvc.perform(
+            MockMvcRequestBuilders
+            .post("/game/connect")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(asJsonString(connectRequest))
+        ).andDo(print()).andExpect(status().isInternalServerError());
+    }
     public static String asJsonString(Object obj) {
         try {
             return new ObjectMapper().writeValueAsString(obj);
